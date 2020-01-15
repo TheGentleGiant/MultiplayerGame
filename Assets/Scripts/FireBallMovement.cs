a20895fefad977086088f7757e61a184bfc6d1ac
Mirel <mirelgrigore@gmail.com>
2020-01-15 10:57:15 +0100

    First Upload

---
+++ Assets/Scripts/FireBallMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class FireBallMovement : NetworkBehaviour
+{
+    public float Speed = 5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward *Speed);
+    }
+}
---
+++ Assets/Scripts/PlayerSimple.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class PlayerSimple : NetworkBehaviour
+{
+    [SerializeField] private GameObject _projectilePrefab = null;
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+    }
+    void Start()
+    {
+        //StartCoroutine(MoveAround());
+    }
+
+ /*   private Vector3 ReturnRandomPos()
+    {
+        return transform.position += new Vector3(Random.Range(-4f, 4f), Random.Range(-4f, 4f), 0f);
+    }
+    public IEnumerator MoveAround()
+    {
+        WaitForSeconds wait = new WaitForSeconds(1f);
+        Vector3 startPosition = transform.position;
+
+        while (true)
+        {
+            transform.position = startPosition + ReturnRandomPos();
+            yield return wait;
+        }
+    }
+    
+*/
+ private void FixedUpdate()
+ {
+     if (Input.GetKey(key: KeyCode.W))
+     {
+         transform.Translate(0, 0, 1f);
+     }
+
+     if (Input.GetKey(key: KeyCode.S))
+     {
+         transform.Translate(0, 0, -1f);
+     }
+
+     if (Input.GetKey(key: KeyCode.D))
+     {
+         transform.Translate(1f, 0, 0);
+     }
+
+     if (Input.GetKey(key: KeyCode.A))
+     {
+         transform.Translate(-1, 0, 0);
+     }
+
+     if (Input.GetMouseButtonDown(0) && hasAuthority)
+     {
+        Cmd_InstantiateNewFireBall();
+     }
+ }
+
+ [Command]
+ private void Cmd_InstantiateNewFireBall()
+ {
+     GameObject p = Instantiate(_projectilePrefab, transform.position,
+         Quaternion.identity);
+     
+     NetworkServer.Spawn(p);
+ }
+ 
+ 
+ 
+ 
+}

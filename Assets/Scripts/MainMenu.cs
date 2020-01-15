7da3aff1dd1a01edf4b460e1b62d28ad2c759cb6
Mirel <mirelgrigore@gmail.com>
2020-01-15 16:23:24 +0100

    Modified Old HUD

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button _HostButton = null;
+    [SerializeField] private Button _ConnectButton = null;
+    [SerializeField] private Button _ServerONlcyButton = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+               
+    }
+
+    public void HostSession()
+    {
+        
+    }
+    
+}

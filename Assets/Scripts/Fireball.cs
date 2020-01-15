d205b15bbecd220d7d54d632bf491346975247af
markusbjork1986 <marcus.bjork2@gmail.com>
2020-01-15 14:37:35 +0100

    Character movement,animations, fireball projectile placeholders

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public GameObject projectile;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject fireball = Instantiate(projectile, transform);
+            Rigidbody rb = fireball.GetComponent<Rigidbody>();
+            fireball.transform.parent = null;
+            rb.velocity = transform.forward * 20f;
+        }
+
+    }
+}

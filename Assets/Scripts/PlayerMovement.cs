5baceda3179a5ed05a74e254b1d11cdb2bbd7a66
Mirel <mirelgrigore@gmail.com>
2020-01-20 15:32:56 +0100

    just some stuff for the enviroment

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
-
+//TODO: sync animations as well 
 public class PlayerMovement : NetworkBehaviour
 {
     public CharacterController controller;
